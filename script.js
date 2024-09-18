f7f86b6db1cb9b0579cf90d8fb39a48e56e2778d
Tiago C. dos Santos <caceraghi@gmail.com>
2024-09-18 10:00:25 -0300

    Versao 0.0.3 - Login

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const loginModal = document.getElementById('loginModal');
+const loginButton = document.getElementById('loginButton');
+const closeModal = document.getElementsByClassName('close')[0];
 
+loginButton.onclick = function() {
+    loginModal.style.display = 'block';
+}
+
+closeModal.onclick = function() {
+    loginModal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target === loginModal) {
+        loginModal.style.display = 'none';
+    }
+}
+
+document.getElementById('loginForm').addEventListener('submit', function(e){
+    ty
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'admin' && password === '1234'){
+        alert('Login bem-sucedido!');
+        loginModal.style.display = 'none';
+    }else {
+        alert('Usuário ou Senha errados!!!');
+    }
+
+})
